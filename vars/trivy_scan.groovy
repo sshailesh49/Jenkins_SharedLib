c08bb24c035b7f76de6b82e7968386ca1310d3b7
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-05-23 17:57:25 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -2,10 +2,10 @@ def call(){
   //sh "trivy fs ."
 
   
-  trivy fs --scanners vuln,secret,config \
+ sh "trivy fs --scanners vuln,secret,config \
                              --severity CRITICAL,HIGH,MEDIUM,LOW \
                              --format table \
                             --output trivy-fs-report.txt \
-                               .
+                               . "
 
 }

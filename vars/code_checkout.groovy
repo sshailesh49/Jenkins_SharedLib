e15847ca6fd5775452feb7db5a32598058752fe6
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-05-23 16:54:14 +0530

    Update code_checkout.groovy

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,3 +1,3 @@
-def call(String GitUrl, String GitBranch, string GitToken){
+def call(String GitUrl, String GitBranch, String GitToken){
   git branch: "${GitBranch}", url: "${GitUrl}", credentialsId: "${GitToken}"
 }

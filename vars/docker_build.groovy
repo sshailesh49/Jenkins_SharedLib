f35c35d2391aa2260b6f5a3a7224911ccc5d5416
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-05-23 18:14:33 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,14 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+//def call(String ProjectName, String ImageTag, String DockerHubUser){
+  // sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+
+def call(){
+  
+    sh 'docker info'
+    sh "docker build --build-arg TMDB_V3_API_KEY=c0da2c6d1a96a38fffd098abd2fad815 -t $IMAGE_NAME ."
+    sh "docker tag $IMAGE_NAME $REPO_IMAGE"
+     sh "docker push $REPO_IMAGE"
+
+
+  
 }

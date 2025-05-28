79530b08d75b09e98e3f159b29e833e35823f101
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-05-28 17:05:03 +0530

    Update dockerscouimgscan.groovy

--- vars/dockerscouimgscan.groovy
+++ vars/dockerscouimgscan.groovy
@@ -1,7 +1,9 @@
-call(){
+def call(){
       sh '''
            export PATH=$HOME/bin:$PATH
           docker-scout quickview $IMAGE_NAME > scout-full-qv.txt || true
           docker-scout cves  $IMAGE_NAME > report-cves.txt 
         '''
+
+
 }

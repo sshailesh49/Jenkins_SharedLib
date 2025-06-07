bbaeb75443cee99c60350469ac39e0c4ff35f7fd
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-06-07 12:31:53 +0530

    Update updateAppWeight.groovy

--- vars/updateAppWeight.groovy
+++ vars/updateAppWeight.groovy
@@ -1,1 +1,15 @@
+//def updateAppV2Weight(String filePath, int newWeight = 100) {
+    // Jenkins Pipeline shell command execute use  sh() 
+//    def sedCommand = "sed -i 's/\\(\"serviceName\": \"app-v2\", \"servicePort\": 80, \"weight\": \\)[0-9]\\+/\\1${newWeight}/' ${filePath}"
+    
+//    echo "Running command: ${sedCommand}"
+    
+//    sh sedCommand
+// }
+
+def call (String fileName ,String appName, int newWeight) {
+  // sh 'sed -i 's/\("serviceName": "app-v2", "servicePort": 80, "weight": \)[0-9]\+/\1100/' ingress-canary.yaml'
+  sh "sed -i 's/\\(\"serviceName\": ${appName}, \"servicePort\": 80, \"weight\": \\)[0-9]\\+/\\1${newWeight}/' ${filePath}"
+
+}
 

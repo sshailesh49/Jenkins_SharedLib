1bc9e291f7de708abf3718ccce37e3dac331191e
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-06-07 13:21:26 +0530

    Update updateAppWeight.groovy

--- vars/updateAppWeight.groovy
+++ vars/updateAppWeight.groovy
@@ -1,8 +1,8 @@
 
 
 def call(){
-   sh 'sed -i 's/\("serviceName": "app-v2", "servicePort": 80, "weight": \)[0-9]\+/\110/' ingress-canary.yaml'
- // sh "sed -i 's/\\(\"serviceName\": ${appName}, \"servicePort\": 80, \"weight\": \\)[0-9]\\+/\\1${newWeight}/' ${filePath}"
+    sh "git --version"
+    sh "pwd"
 
 }
 
